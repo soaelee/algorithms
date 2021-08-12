0c63c9b6078147896cb85626921fe377750d31ef
soaelee <soae0923@gmail.com>
2021-08-12 22:37:51 +0900

    투포인터 마무리 (+prettier설정으로 인한 수정)

--- section3/question3.js
+++ section3/question3.js
@@ -1,16 +1,16 @@
 //문자열에서 자연수만들기
-const solution = (str) => {
-	str = str.replace(/[^0-9]/g, "");
+const solution = str => {
+	str = str.replace(/[^0-9]/g, '');
 	return +str;
 };
 
-const str = "g0en2T0s8eSoft";
-const str2 = "tge0a1h205er";
+const str = 'g0en2T0s8eSoft';
+const str2 = 'tge0a1h205er';
 console.log(solution(str2));
 
 //isNaN이용하기(Not a Number : 매개변수가 숫자인지 검사)
-const solutionT = (str) => {
-	let answer = "";
+const solutionT = str => {
+	let answer = '';
 	for (let i of str) {
 		if (!isNaN(i)) answer += i;
 	}
